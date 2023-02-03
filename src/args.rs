acf0603e5f2d808bdffafcc5013b5c9f947f6715
Sxmon17 <simonguglberger@gmail.com>
2023-02-03 10:39:52 +0100

    fixed help page and made chunk-type optional

--- src/args.rs
+++ src/args.rs
@@ -3,43 +3,43 @@ use std::path::PathBuf;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
+#[command(name = "png-texter", about = "A simple png chunk message encoder/decoder")]
+#[command(author = "Simon Guglberger <simonguglberger@gmail.com>")]
+#[command(version = "v1.0.1-alpha")]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
     #[command(subcommand)]
     pub command: Option<Commands>,
+
+    /// Sets the chunk type to use
+    #[arg(short, long, value_name = "CHUNK_TYPE")]
+    pub chunk_type: Option<String>,
 }
 
 #[derive(Subcommand)]
 pub enum Commands {
+    /// encode the png with a secret message
     Encode {
         #[arg(index = 1, value_name = "FILE")]
-        // Png to encode
+        /// Png to encode
         png: Option<PathBuf>,
 
-        // Chunk type to use
-        #[arg(index = 2, value_name = "CHUNK_TYPE")]
-        chunk_type: Option<String>,
-
-        // Secret message to encode within the png
-        #[arg(index = 3, value_name = "SECRET_MSG")]
+        /// Secret message to encode within the png
+        #[arg(index = 2, value_name = "SECRET_MSG")]
         secret_msg: Option<String>,
     },
+
+    /// decode the png to get the secret message
     Decode {
-        // Png to decode
+        /// Png to decode
         #[arg(index = 1, value_name = "FILE")]
         png: Option<PathBuf>,
-
-        // Chunk type to use
-        #[arg(index = 2, value_name = "CHUNK_TYPE")]
-        chunk_type: Option<String>,
     },
+
+    /// remove the chunk from the png
     Remove {
-        // Png to use
+        /// Png to use
         #[arg(index = 1, value_name = "FILE")]
         png: Option<PathBuf>,
-
-        // Chunk type to remove
-        #[arg(index = 2, value_name = "CHUNK_TYPE")]
-        chunk_type: Option<String>,
     },
 }
--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,25 @@ use clap::Parser;
 fn main() {
     let cli = Args::parse();
 
+    let chunk_type = Some("rust");
+
     match &cli.command {
         Some(Commands::Encode {
             png,
-            chunk_type,
             secret_msg,
         }) => {
             encode(
                 png.as_ref().unwrap(),
-                chunk_type.as_ref().unwrap(),
+                 chunk_type.unwrap(),
                 secret_msg.as_ref().unwrap(),
             )
             .expect("Error while encoding");
         }
-        Some(Commands::Decode { png, chunk_type }) => {
-            decode(png.as_ref().unwrap(), chunk_type.as_ref().unwrap()).unwrap();
+        Some(Commands::Decode { png }) => {
+            decode(png.as_ref().unwrap(), chunk_type.unwrap()).unwrap();
         }
-        Some(Commands::Remove { png, chunk_type }) => {
-            remove(png.as_ref().unwrap(), chunk_type.as_ref().unwrap()).unwrap();
+        Some(Commands::Remove { png }) => {
+            remove(png.as_ref().unwrap(), chunk_type.unwrap()).unwrap();
         }
         None => {
             println!("No subcommand was used");
