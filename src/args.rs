93babf2883eba2772122a80fd94c4e27c1d9f3ad
Sxmon17 <simonguglberger@gmail.com>
2023-02-03 07:15:32 +0100

    cleaned up commands

--- src/args.rs
+++ src/args.rs
@@ -5,11 +5,6 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    pub name: Option<String>,
-
-    #[arg(short, long, value_name = "FILE")]
-    pub config: Option<PathBuf>,
-
     #[command(subcommand)]
     pub command: Option<Commands>,
 }
@@ -18,25 +13,32 @@ pub struct Args {
 pub enum Commands {
     Encode {
         #[arg(index = 1, value_name = "FILE")]
+        // Png to encode
         png: Option<PathBuf>,
 
+        // Chunk type to use
         #[arg(index = 2, value_name = "CHUNK_TYPE")]
         chunk_type: Option<String>,
 
+        // Secret message to encode within the png
         #[arg(index = 3, value_name = "SECRET_MSG")]
         secret_msg: Option<String>,
     },
     Decode {
+        // Png to decode
         #[arg(index = 1, value_name = "FILE")]
         png: Option<PathBuf>,
 
+        // Chunk type to use
         #[arg(index = 2, value_name = "CHUNK_TYPE")]
         chunk_type: Option<String>,
     },
     Remove {
+        // Png to use
         #[arg(index = 1, value_name = "FILE")]
         png: Option<PathBuf>,
 
+        // Chunk type to remove
         #[arg(index = 2, value_name = "CHUNK_TYPE")]
         chunk_type: Option<String>,
     },
